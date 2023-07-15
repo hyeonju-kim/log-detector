6e4c0968b384153159925e1a5216a2678fba4e49
hyeonju-kim <yocu1784@naver.com>
2023-07-15 09:12:59 +0900

    [230715] controller추가

--- src/main/java/internal/system/logdetector/LogController.java
+++ src/main/java/internal/system/logdetector/LogController.java
@@ -1,5 +1,4 @@
 package internal.system.logdetector;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
